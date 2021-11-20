aa3db0fe5ae953ae5bb6d74d42823f6417522fa1
Viviannie De La Fuente <Viviannie@Viviannies-MacBook-Pro.local>
2021-11-20 02:17:24 -0600

    2

--- index.php
+++ index.php
@@ -101,6 +101,7 @@
   <!--End of Cart-->
   <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
   <script src="app.js"></script>
+  <script src="script.js"></script>
 
 
 </body>
