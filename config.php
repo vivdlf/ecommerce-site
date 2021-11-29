dbf0c8f1664b83f84daf8f5779e7599bdd8f173f
Viviannie De La Fuente <Viviannie@Viviannies-MacBook-Pro.local>
2021-11-29 15:48:36 -0600

    fixes to database config file

--- config.php
+++ config.php
@@ -1,27 +1,18 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-// define('DB_SERVER', 'localhost');
-// define('DB_USERNAME', 'viv');
-// define('DB_PASSWORD', 'viv');
-// define('DB_NAME', 'fleex');
+define('DB_SERVER', 'us-cdbr-east-04.cleardb.com');
+define('DB_USERNAME', 'b29224117890cd');
+define('DB_PASSWORD', '2556331d');
+define('DB_NAME', 'heroku_acb619cffe4521e');
 
 /* Attempt to connect to MySQL database */
-//$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 // Check connection
-//if($mysqli === false){
-// die("ERROR: Could not connect. " . $mysqli->connect_error);
-//}
+if($mysqli === false){
+die("ERROR: Could not connect. " . $mysqli->connect_error);
+}
 
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"], 1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
 ?>
